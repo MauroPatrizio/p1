a47580ecf022bb83e163b9c8352a114b21b86849
Mauro Patrizio <maurodanielpatrizio@gmail.com>
2026-07-10 18:57:44 -0300

    router instalado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function ProtectedRoute({ isAuthenticated, children }) {
+	if (!isAuthenticated) {
+		return (
+			<Navigate
+				to="/login"
+				replace
+			/>
+		);
+	}
+
+	return children;
+}
 
 function App() {
-	return <Home />;
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/login"
+					element={<Login onLogin={() => setIsAuthenticated(true)} />}
+				/>
+				<Route
+					path="/home"
+					element={
+						<ProtectedRoute isAuthenticated={isAuthenticated}>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="*"
+					element={
+						<Navigate
+							to="/login"
+							replace
+						/>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
